be186e22071216f588396e26cd3e0fc3cb5eb1fa
Cauã Gilberto <cauagilbertop@outlook.com>
2025-08-25 18:40:09 -0300

    date verify

--- todolist/src/main/java/br/com/cauagilberto/todolist/task/taskController.java
+++ todolist/src/main/java/br/com/cauagilberto/todolist/task/taskController.java
@@ -1,8 +1,11 @@
 package br.com.cauagilberto.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,10 +24,21 @@ public class taskController {
     
     
     @PostMapping("/")
-    public taskModel create(@RequestBody taskModel taskModel, HttpServletRequest request){
+    public ResponseEntity create(@RequestBody taskModel taskModel, HttpServletRequest request){
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID) idUser);
+
+        var currentDate = LocalDateTime.now();
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de início / data de término não pode ser uma data passada.");
+        }
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de término não pode ser anterior a data de início.");
+        }
+
         var task = this.taskRepo.save(taskModel);
-        return task;
+        return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 }
